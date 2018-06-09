07d2b7424c00aba0e36048d809c071c1187a8677
josef utleg <josefutleg@gmail.com>
2018-06-08 22:24:31 -0700

    solved reset guesses counter and guesses array. created secret hint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,59 @@
-//create an array with all of the letters
 var alphabet = ['a','b','c','d','e','f','g','h','i','j',
                 'k','l','m','n','o','p','q','r','s','t',
                 'u','v','w','x','y','z']
-    
-//
+
 var guesses = [];
 
 var winCounter = 0;
 var lossCounter = 0;
 var guessesCounter = 9;
-var userGuessesContainer ='';
+var hint = 'I';
 var randomLetter = Math.floor(Math.random()*alphabet.length);
-
-
+var resetGuesses =" ";
 
 document.getElementById('wins').innerText = winCounter;
 document.getElementById('losses').innerText = winCounter;
 document.getElementById('remainingGuesses').innerText = guessesCounter;
 
-function startUp(){
-    for(var i = 0; i < randomLetter.length; i++){
-    }
-}
 console.log(alphabet[randomLetter]);
 
 function play(x){
-    if(x.key !== alphabet[randomLetter]){
-        guesses.push(x.key);
+    var userInput = x.key;
+    guessesCounter = guessesCounter -1;
+    if(userInput !== (alphabet[randomLetter])) {
+        guesses.push(userInput);
         document.getElementById('userGuesses').innerText = guesses;
-        guessesCounter = guessesCounter - 1
         document.getElementById('remainingGuesses').innerText = guessesCounter;
-        console.log('false');
     }
-    if (x.key == alphabet[randomLetter]) {
-        winCounter = winCounter + 1
-        document.getElementById('wins').innerText = winCounter;
-        var resetGuesses =" ";
+    if (userInput == (alphabet[randomLetter])){
+        randomLetter = Math.floor(Math.random()*alphabet.length);
+        var computerAlphabet = alphabet[randomLetter];
+        console.log(computerAlphabet);
+        guessesCounter = 9;
+        resetGuesses = ' ';
+        guesses = [];
+        hint = 'I';
+        winCounter++;
+        document.getElementById('remainingGuesses').innerText = guessesCounter;
         document.getElementById('userGuesses').innerText = resetGuesses;
-
+        document.getElementById('wins').innerText = winCounter;
+        document.getElementById('hint').innerHTML = hint;
     }
-    if (guessesCounter <= 0){
-        startUp();
-        lossCounter = lossCounter +1;
+    if (guessesCounter == 1){
+        hint = alphabet[randomLetter];
+        document.getElementById('hint').innerHTML = hint;
+    }
+    if (guessesCounter == 0){
+        guessesCounter = 9;
+        resetGuesses = ' ';
+        lossCounter++;
+        guesses = [];
+        hint = 'I';
+        document.getElementById('remainingGuesses').innerText = guessesCounter;
+        document.getElementById('userGuesses').innerText = resetGuesses;
         document.getElementById('losses').innerText = lossCounter;
-        var resetLossesCounter = 9;
-        document.getElementById('remainingGuesses').innerText = resetLossesCounter;
-        var resetUserGuesses = " ";
-        document.getElementById('userGueses')
-        .innerText = resetUserGuesses;
+        document.getElementById('hint').innerHTML = hint;
     }
-
 }
-
 
 document.onkeyup = play;
